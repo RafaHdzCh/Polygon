8a7c22581d5a2039011af12c7284e5889ecf8fd4
RafaHdzCh <64990211+RafaHdzCh@users.noreply.github.com>
2022-10-05 17:09:55 -0500

    Update UI Texts and Coin

--- Assets/Coin/Coin.cs
+++ Assets/Coin/Coin.cs
@@ -1,32 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Coin : MonoBehaviour
 
 {
-	public static int coinsCount = 0; //Variable global y estatica (Una para todos)
+	public static int coinsCount = 0;
+    [SerializeField] TextMeshProUGUI coinsCollected;
+    [SerializeField] AudioSource coinSound;
 
     void Start()
     {
-		Coin.coinsCount++;
-        Debug.Log("El juego ha comenzado y ahora hay " + coinsCount + " monedas.");
+		coinsCount++;
     }
 
     void Update()
     {
-    	
+        coinsCollected.text = coinsCount.ToString();
     }
 
     private void OnTriggerEnter (Collider otherCollider)
     {
     	if (otherCollider.CompareTag("Player")==true)
     	{
-    		Coin.coinsCount--;
-    		Debug.Log("El jugador ha tomado una moneda y ahora hay " + coinsCount + " monedas." );
-    			if (Coin.coinsCount==0)
+            coinSound.Play();
+            coinsCount--;
+    			if (coinsCount==0)
     			{
-    				Debug.Log("El juego ha terminado.");
     				GameObject gameManager = GameObject.Find("GameManager");
     				Destroy (gameManager);
     				GameObject[] fireworksSystem = GameObject.FindGameObjectsWithTag("Fireworks");
--- Assets/Coin/Timer.cs
+++ Assets/Coin/Timer.cs
@@ -1,35 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
 
 	public Text tiempoText;
-	public float maxTime = 300f;
-	private float countdown = 0f;
+	public float maxTime = 180.0f;
+	private float countdown = 0.0f;
+    [SerializeField] TextMeshProUGUI timer;
 
     void Start()
     {
-        countdown=maxTime;
+        countdown = maxTime;
     }
 
     void Update()
     {
-        countdown=countdown-Time.deltaTime; //Deltatime=Tiempo desde que se renderizo en pantalla el ultimo frame.
-        Debug.Log("Cuenta atras: " + countdown);
+        countdown = countdown - Time.deltaTime; //Deltatime=Tiempo desde que se renderizo en pantalla el ultimo frame.
+        timer.text = countdown.ToString("F0");
         if(countdown<=0)
        		{
-        		Debug.Log("Te has quedado sin tiempo.");
         		Coin.coinsCount=0;
         		SceneManager.LoadScene("MainScene");
         	}
-    }
-
-    void FixedUpdate()
-    {
-
     }
 }
